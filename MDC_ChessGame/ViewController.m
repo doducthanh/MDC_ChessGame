93d869a46f11a09300d56a3cc4c6b98167b3f129
Duc Thanh <ducthanh@ducs-mac.local>
2017-03-16 14:31:30 +0700

    Initial Commit

---
+++ MDC_ChessGame/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MDC_ChessGame
+//
+//  Created by Duc Thanh on 16/03/2017.
+//  Copyright © 2017 Duc Thanh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ MDC_ChessGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MDC_ChessGame
+//
+//  Created by Duc Thanh on 16/03/2017.
+//  Copyright © 2017 Duc Thanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
